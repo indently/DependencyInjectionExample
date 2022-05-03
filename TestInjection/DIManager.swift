26d5d0668c99d93890f1147a92d5bef50856b1d2
Federico <federico@fastmail.com>
2022-05-03 11:33:38 +0200

    Added more documentation

--- TestInjection/DIManager.swift
+++ TestInjection/DIManager.swift
@@ -11,6 +11,7 @@ import Foundation
 struct DependencyInjector {
     private static var dependencyList: [String:Any] = [:]
     
+    /// Attempts to retrieve the dependency. If it doesn't exist, it's an error in our logic, so we throw a fatal error.
     static func resolve<T>() -> T {
         guard let t = dependencyList[String(describing: T.self)] as? T else {
             fatalError("No provider registered for type \(T.self)")
@@ -18,12 +19,13 @@ struct DependencyInjector {
         return t
     }
     
+    /// Creates a "key:value" in a dictionary with the description of T being the key, and T being the actual value.
     static func register<T>(dependency: T) {
         dependencyList[String(describing: T.self)] = dependency
     }
 }
 
-
+/// Property wrapper to simplify injection
 @propertyWrapper struct Inject<T> {
     var wrappedValue: T
     
@@ -33,6 +35,7 @@ struct DependencyInjector {
     }
 }
 
+/// Property wrapper to simplify providing
 @propertyWrapper struct Provider<T> {
     var wrappedValue: T
     
